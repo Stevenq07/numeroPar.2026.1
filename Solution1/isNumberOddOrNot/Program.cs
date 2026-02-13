c3da86d8b14c4e84c549740dec7d49929ce13e9e
Anthonia Quiroz <antho072021@outlook.es>
2026-02-12 19:06:17 -0500

    even or odd development

---
+++ Solution1/isNumberOddOrNot/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Ingrese numero :");
+var numberString = Console.ReadLine();
+var numberInt = int.Parse(numberString!);
+if (numberInt % 2 == 0)
+{
+    Console.WriteLine($"el numero : {numberInt} es par");
+}
+else
+{
+    Console.WriteLine($"el numero: {numberInt} es impar");
+}
+
+
+
+
